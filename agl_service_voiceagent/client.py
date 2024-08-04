9e7d497bc025b6e41d7662e7e8935b5d23211ec2
Anuj Solanki <anuj603362@gmail.com>
2024-08-04 16:59:15 +0530

    Implemented auto-mode
    Change-Id: I9a16724767fa497007473d64ba7c881961cd40f7

--- agl_service_voiceagent/client.py
+++ agl_service_voiceagent/client.py
@@ -58,8 +58,56 @@ def run_client(server_address, server_port, action, mode, nlu_engine, recording_
                 stub = voice_agent_pb2_grpc.VoiceAgentServiceStub(channel=channel)
                 stt_framework = voice_agent_pb2.VOSK if stt_framework == "vosk" else voice_agent_pb2.WHISPER
                 online_mode = voice_agent_pb2.ONLINE if online_mode == True else voice_agent_pb2.OFFLINE
-                print("[+] Recording voice command in auto mode...")
-                record_start_request = voice_agent_pb2.RecognizeVoiceControl(action=voice_agent_pb2.START, nlu_model=nlu_engine, record_mode=voice_agent_pb2.AUTO, stt_framework=stt_framework,online_mode=online_mode,)
+                while(True):
+                    wake_request = voice_agent_pb2.Empty()
+                    wake_results = stub.DetectWakeWord(wake_request)
+                    wake_word_detected = False
+                    for wake_result in wake_results:
+                        print("Wake word status: ", wake_word_detected)
+                        if wake_result.status:
+                            print("Wake word status: ", wake_result.status)
+                            wake_word_detected = True
+                            break
+                    print("Wake word detected: ", wake_word_detected)
+                    if wake_word_detected:
+                        print("[+] Wake Word detected! Recording voice command...")
+                        record_start_request = voice_agent_pb2.RecognizeVoiceControl(action=voice_agent_pb2.START, nlu_model=nlu_engine, record_mode=voice_agent_pb2.MANUAL, stt_framework=stt_framework,online_mode=online_mode,)
+                        response = stub.RecognizeVoiceCommand(iter([record_start_request]))
+                        stream_id = response.stream_id
+
+                        time.sleep(recording_time) # pause here for the number of seconds passed by user or default 5 seconds
+
+                        record_stop_request = voice_agent_pb2.RecognizeVoiceControl(action=voice_agent_pb2.STOP, nlu_model=nlu_engine, record_mode=voice_agent_pb2.MANUAL, stream_id=stream_id,stt_framework=stt_framework,online_mode=online_mode,)
+                        record_result = stub.RecognizeVoiceCommand(iter([record_stop_request]))
+                        print("[+] Voice command recording ended!")
+
+                        status = "Uh oh! Status is unknown."
+                        if record_result.status == voice_agent_pb2.REC_SUCCESS:
+                            status = "Yay! Status is success."
+                        elif record_result.status == voice_agent_pb2.VOICE_NOT_RECOGNIZED:
+                            status = "Voice not recognized."
+                        elif record_result.status == voice_agent_pb2.INTENT_NOT_RECOGNIZED:
+                            status = "Intent not recognized."
+
+                        # Process the response
+                        print("Status:", status)
+                        print("Command:", record_result.command)
+                        print("Intent:", record_result.intent)
+                        intent_slots = []
+                        for slot in record_result.intent_slots:
+                            print("Slot Name:", slot.name)
+                            print("Slot Value:", slot.value)
+                            i_slot = voice_agent_pb2.IntentSlot(name=slot.name, value=slot.value)
+                            intent_slots.append(i_slot)
+
+                        if record_result.status == voice_agent_pb2.REC_SUCCESS:
+                            print("[+] Executing voice command...")
+                            exec_voice_command_request = voice_agent_pb2.ExecuteInput(intent=record_result.intent, intent_slots=intent_slots)
+                            response = stub.ExecuteCommand(exec_voice_command_request)
+                            print("Response:", response)
+                    wake_word_detected = False
+                    time.sleep(1)
+
                 
 
             elif mode == 'manual':
